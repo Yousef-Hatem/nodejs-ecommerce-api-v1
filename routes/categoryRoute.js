a63d2a02fb0b843ea10e072df036e2d9a19ad400
Yousef Hatem <yousef26hatem@gmail.com>
2023-02-09 23:48:27 +0200

    Get specific category by id

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { getCategories, createCategory } = require("../services/categoryService");
+const { getCategories, createCategory, getCategory } = require("../services/categoryService");
 
 const router = express.Router();
 
 router.route('/').get(getCategories).post(createCategory);
+router.route('/:id').get(getCategory);
 
 module.exports = router;
