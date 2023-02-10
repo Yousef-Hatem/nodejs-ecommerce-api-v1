57b105f3f2b187756d98927d498b39803c8b1d63
Yousef Hatem <yousef26hatem@gmail.com>
2023-02-10 23:02:17 +0200

    Product model

---
+++ models/productModel.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "Too short product title"],
+      maxlength: [100, "Too long product title"],
+    },
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+      minlength: [20, "Too short product description"],
+      maxlength: [1000, "Too long product description"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Product quantity is required"],
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      trim: true,
+      max: [100000000000, "Too long product price"],
+    },
+    priceAfterDiscount: {
+      type: Number,
+    },
+    colors: [String],
+    imageCover: {
+      type: String,
+      required: [true, "Product image cover is required"],
+    },
+    images: [String],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Product category is required"],
+    },
+    subcategory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubCategory",
+      },
+    ],
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+    },
+    ratingsAverage: {
+      type: Number,
+      min: [1, "Rating must be above or equal 1.0"],
+      max: [5, "Rating must be below or equal 5.0"],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
