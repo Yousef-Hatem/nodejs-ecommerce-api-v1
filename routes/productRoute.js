73823e1d1e09c16f46f0e35afcfa1b5045d27812
Yousef Hatem <yousef26hatem@gmail.com>
2023-02-10 23:10:57 +0200

    Product route

---
+++ routes/productRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+  getProductValidator,
+  createProductValidator,
+  updateProductValidator,
+  deleteProductValidator,
+} = require("../utils/validators/productValidator");
+
+const {
+  getProducts,
+  createProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../services/productService");
+
+const router = express.Router();
+
+router.route("/").get(getProducts).post(createProductValidator, createProduct);
+router
+  .route("/:id")
+  .get(getProductValidator, getProduct)
+  .put(updateProductValidator, updateProduct)
+  .delete(deleteProductValidator, deleteProduct);
+
+module.exports = router;
