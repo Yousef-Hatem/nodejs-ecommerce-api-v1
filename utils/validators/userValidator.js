9f6703045f31dbfc0d7a77a7cb6e1497e85a01ea
Yousef Hatem <yousef26hatem@gmail.com>
2023-02-12 15:39:57 +0200

    User validation layer

---
+++ utils/validators/userValidator.js
@@ -0,0 +1,66 @@
+const { check, body } = require("express-validator");
+const slugify = require("slugify");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const User = require("../../models/userModel");
+
+exports.createUserValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("User required")
+    .isLength({ min: 3 })
+    .withMessage("Too short user name")
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+
+  check("email")
+    .notEmpty()
+    .withMessage("Email required")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .custom((val) =>
+      User.findOne({ email: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error("E-mail already in user"));
+        }
+      })
+    ),
+
+  check("password")
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+
+  check("phone")
+    .optional()
+    .isMobilePhone(["ar-EG", "ar-SA"])
+    .withMessage("Invalid phone number only accepted Egy and SA Phone numbers"),
+
+  check("profileImg").optional(),
+  check("role").optional(),
+
+  validatorMiddleware,
+];
+
+exports.getUserValidator = [
+  check("id").isMongoId().withMessage("Invalid user id format"),
+  validatorMiddleware,
+];
+
+exports.updateUserValidator = [
+  check("id").isMongoId().withMessage("Invalid user id format"),
+  body("name")
+    .optional()
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  validatorMiddleware,
+];
+
+exports.deleteUserValidator = [
+  check("id").isMongoId().withMessage("Invalid user id format"),
+  validatorMiddleware,
+];
