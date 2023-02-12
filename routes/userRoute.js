bbb56b19b961c8e4ad7ce4883fdd2a0b24adbec1
Yousef Hatem <yousef26hatem@gmail.com>
2023-02-12 14:44:25 +0200

    User Service & Route

---
+++ routes/userRoute.js
@@ -0,0 +1,28 @@
+const express = require("express");
+// const {
+//   getUserValidator,
+//   createUserValidator,
+//   updateUserValidator,
+//   deleteUserValidator,
+// } = require("../utils/validators/userValidator");
+
+const {
+  getUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  uploadUserImage,
+  resizeImage,
+} = require("../services/userService");
+
+const router = express.Router();
+
+router.route("/").get(getUsers).post(uploadUserImage, resizeImage, createUser);
+router
+  .route("/:id")
+  .get(getUser)
+  .put(uploadUserImage, resizeImage, updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
---
+++ services/userService.js
@@ -0,0 +1,48 @@
+const asyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
+const sharp = require("sharp");
+
+const factory = require("./handlersFactory");
+const { uploadSingleImage } = require("../middlewares/uploadimageMiddleware");
+const User = require("../models/userModel");
+
+exports.uploadUserImage = uploadSingleImage("profileImg");
+
+exports.resizeImage = asyncHandler(async (req, res, next) => {
+  const filename = `user-${uuidv4()}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(600, 600)
+    .toFormat("jpeg")
+    .jpeg({ quality: 95 })
+    .toFile(`uploads/users/${filename}`);
+
+  req.body.profileImg = filename;
+
+  next();
+});
+
+// @desc    Get list of user
+// @route   GET /api/v1/users
+// @access  Private
+exports.getUsers = factory.getAll(User);
+
+// @desc    Get specific user by id
+// @route   GET /api/v1/users/:id
+// @access  Private
+exports.getUser = factory.getOne(User);
+
+// @desc    Create user
+// @route   POST /api/v1/users
+// @access  Private
+exports.createUser = factory.createOne(User);
+
+// @desc    Update specific user
+// @route   PUT /api/v1/users/:id
+// @access  Private
+exports.updateUser = factory.updateOne(User);
+
+// @desc    Delete specific user
+// @route   DELETE /api/v1/users:id
+// @access  Private
+exports.deleteUser = factory.deleteOne(User);
