484ed6fb37d5d0a72f737da105f9bf9f27819570
Yousef Hatem <yousef26hatem@gmail.com>
2023-02-17 05:38:14 +0200

    Forgot Password: Verify reset code

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,12 +4,18 @@ const {
   loginValidator,
 } = require("../utils/validators/authValidator");
 
-const { signup, login, forgotPassword } = require("../services/authService");
+const {
+  signup,
+  login,
+  forgotPassword,
+  verifyPassResetCode,
+} = require("../services/authService");
 
 const router = express.Router();
 
 router.post("/signup", signupValidator, signup);
 router.post("/login", loginValidator, login);
 router.post("/forgotPassword", forgotPassword);
+router.post("/verifyResetCode", verifyPassResetCode);
 
 module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -153,3 +153,27 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     .status(200)
     .json({ status: "Success", message: "Reset code sent to email" });
 });
+
+exports.verifyPassResetCode = asyncHandler(async (req, res, next) => {
+  const hashedRestCode = crypto
+    .createHash("sha256")
+    .update(req.body.resetCode)
+    .digest("hex");
+
+  const user = await User.findOne({
+    passwordResetCode: hashedRestCode,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new ApiError("Reset code invalid or expired"));
+  }
+
+  user.passwordResetVerified = true;
+
+  await user.save();
+
+  res.status(200).json({
+    status: "Success",
+  });
+});
