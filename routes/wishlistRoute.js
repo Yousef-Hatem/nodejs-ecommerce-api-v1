35531c0abbcffd1ae30da6d24f71c994a0c5889b
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-20 02:32:12 +0300

    Remove Product From Wishlist Handler And Route

--- routes/wishlistRoute.js
+++ routes/wishlistRoute.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const authService = require("../services/authService");
 
-const { addProductToWishlist } = require("../services/wishlistService");
+const {
+  addProductToWishlist,
+  removeProductFromWishlist,
+} = require("../services/wishlistService");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .post(
-    authService.protect,
-    authService.allowedTo("user"),
-    addProductToWishlist
-  );
+router.post(
+  "/",
+  authService.protect,
+  authService.allowedTo("user"),
+  addProductToWishlist
+);
+
+router.delete(
+  "/:productId",
+  authService.protect,
+  authService.allowedTo("user"),
+  removeProductFromWishlist
+);
 
 module.exports = router;
--- services/wishlistService.js
+++ services/wishlistService.js
@@ -18,3 +18,19 @@ exports.addProductToWishlist = asyncHandler(async (req, res) => {
     data: user.wishlist,
   });
 });
+
+exports.removeProductFromWishlist = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $pull: { wishlist: req.params.productId },
+    },
+    { new: true }
+  );
+
+  res.status(200).json({
+    status: "success",
+    message: "Product removed successfully from your wishlist.",
+    data: user.wishlist,
+  });
+});
