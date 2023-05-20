f6641045dde3aae0ed592e7ceced1728d4155b29
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-20 19:45:55 +0300

    Refactor Mount Routes Middleware

--- server.js
+++ server.js
@@ -9,16 +9,8 @@ const ApiError = require("./utils/apiError");
 const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
 
-const categoryRoute = require("./routes/categoryRoute");
-const subCategoryRoute = require("./routes/subCategoryRoute");
-const brandRoute = require("./routes/brandRoute");
-const productRoute = require("./routes/productRoute");
-const userRoute = require("./routes/userRoute");
-const authRoute = require("./routes/authRoute");
-const reviewRoute = require("./routes/reviewRoute");
-const wishlistRoute = require("./routes/wishlistRoute");
-const addressRoute = require("./routes/addressRoute");
-const couponRoute = require("./routes/couponRoute");
+// Routes
+const mountRoutes = require("./routes");
 
 // Connect with db
 dbConnection();
@@ -36,16 +28,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Mount Routes
-app.use("/api/v1/categories", categoryRoute);
-app.use("/api/v1/brands", brandRoute);
-app.use("/api/v1/subcategories", subCategoryRoute);
-app.use("/api/v1/products", productRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/wishlist", wishlistRoute);
-app.use("/api/v1/addresses", addressRoute);
-app.use("/api/v1/coupons", couponRoute);
+mountRoutes(app);
 
 app.all("*", (req, res, next) => {
   next(new ApiError(`Con't find this route: ${req.originalUrl}`, 400));
