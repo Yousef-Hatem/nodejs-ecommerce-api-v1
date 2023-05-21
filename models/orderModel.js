d1604eaa6c1df1da7cdc00decc6836db38ba5074
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-21 15:34:58 +0300

    Order Schema & Model

---
+++ models/orderModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.OrderSchema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Order must be belong to user"],
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+    totalOrderPrice: {
+      type: Number,
+    },
+    paymentMethodType: {
+      type: String,
+      enum: ["card", "cash"],
+      default: "cash",
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
