44bef885dbf082e633f3af9fc6b5c0e445025088
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-21 17:46:21 +0300

    Update Order Status (Admin)

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,6 +5,8 @@ const {
   findAllOrders,
   findSpecificOrder,
   filterOrderForLoggedUser,
+  updateOrderToPaid,
+  updateOrderToDelivered,
 } = require("../services/orderService");
 
 const router = express.Router();
@@ -14,7 +16,13 @@ const authService = require("../services/authService");
 router.use(authService.protect);
 
 router.post("/:cartId", authService.allowedTo("user"), crateCashOrder);
+
 router.get("/", filterOrderForLoggedUser, findAllOrders);
 router.get("/:id", findSpecificOrder);
+
+router.use(authService.allowedTo("admin", "manager"));
+
+router.put("/:id/pay", updateOrderToPaid);
+router.put("/:id/deliver", updateOrderToDelivered);
 
 module.exports = router;
--- services/orderService.js
+++ services/orderService.js
@@ -51,3 +51,41 @@ exports.filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
 exports.findAllOrders = factory.getAll(Order);
 
 exports.findSpecificOrder = factory.getOne(Order);
+
+exports.updateOrderToPaid = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ApiError(
+        `There is no such a order with this id: ${req.params.id}`,
+        404
+      )
+    );
+  }
+
+  order.isPaid = true;
+  order.paidAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json({ status: "success", data: updatedOrder });
+});
+
+exports.updateOrderToDelivered = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ApiError(
+        `There is no such a order with this id: ${req.params.id}`,
+        404
+      )
+    );
+  }
+
+  order.isDelivered = true;
+  order.deliveredAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json({ status: "success", data: updatedOrder });
+});
