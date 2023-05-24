4ff318b43f9dab6502438aa1003047e3694effc2
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-24 06:38:52 +0300

    Fix Card Order Issue

--- services/orderService.js
+++ services/orderService.js
@@ -134,7 +134,7 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 const createCardOrder = async (session) => {
   const cartId = session.client_reference_id;
   const shippingAddress = session.metadata;
-  const orderPrice = session.display_item[0].amount / 100;
+  const orderPrice = session.amount_total / 100;
 
   const cart = await Cart.findById(cartId);
   const user = await User.findOne({ email: session.customer_email });
