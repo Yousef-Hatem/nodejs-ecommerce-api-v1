b165759b9e5a085374f6707d36b347af1870bdf1
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-24 05:19:28 +0300

    Test logs server

--- services/orderService.js
+++ services/orderService.js
@@ -131,6 +131,8 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 });
 
 exports.webhookCheckout = asyncHandler(async (req, res) => {
+  console.log("Starting webhook");
+
   const sig = req.headers["stripe-signature"];
 
   let event;
@@ -144,6 +146,8 @@ exports.webhookCheckout = asyncHandler(async (req, res) => {
   } catch (err) {
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
+
+  console.log("Event Type");
 
   if (event.type === "checkout.session.completed") {
     console.log("Create Order Here.....");
